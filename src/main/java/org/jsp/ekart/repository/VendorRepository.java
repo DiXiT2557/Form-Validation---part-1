5ed146d676fd07708d4618cd51afa047a50345ef
DiXit2557 <dixittank2557@gmail.com>
2024-12-21 16:20:48 +0530

    form velidation completed

--- src/main/java/org/jsp/ekart/repository/VendorRepository.java
+++ src/main/java/org/jsp/ekart/repository/VendorRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendorRepository extends JpaRepository<Vendor, Integer>{
 
+	boolean existsByEmail(String email);
+
+	boolean existsByMobile(long mobile);
+	
 }
